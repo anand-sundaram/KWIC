972662cd56bbdd75824fba7e7ab77f051adef93f
anand-sundaram <anand.sundaram@nus.edu.sg>
2015-08-29 23:15:40 +0800

    Added structure and basic function

--- src/shared/MasterControl.java
+++ src/shared/MasterControl.java
@@ -1,5 +1,33 @@
 package shared;
 
+import java.util.StringTokenizer;
+
 public class MasterControl {
 
+	public static String[] wordsToIgnore = {"is", "the", "of", "and", "as", "a", "after"};
+	public static String[] titles = {"The Day after Tomorrow", "Fast and Furious", "Man of Steel"};
+	public static String[][] titlesByWords;
+	
+	public static void main(String[] args) {
+		
+	}
+	static void titleSplitter() {
+		titlesByWords = new String[titles.length][];
+		for (int x = 0; x < titles.length; x++) {
+			StringTokenizer str = new StringTokenizer(titles[x]);
+			titlesByWords[x] = new String[str.countTokens()];
+			for(int y = 0; y < str.countTokens(); y++) {
+				titlesByWords[x][y] = str.nextToken();
+			}
+		}
+	}
+	static void circularShifter() {
+		
+	}
+	static void alphabetizer() {
+		
+	}
+	static void output() {
+		
+	}
 }
