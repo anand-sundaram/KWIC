957b53bbf7c849a1b0c46d062839ee4886e990f8
rupali <rupali@test.com>
2015-08-29 12:27:32 +0800

    added most of the implemmetation

--- src/shared/KWIC.java
+++ src/shared/KWIC.java
@@ -3,17 +3,91 @@
  * and Object-Oriented Organisation" design style
  */
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.ArrayList;
 
 public class KWIC 
 {
 	String [] sentence;
+	ArrayList<String> possibleShifts = new ArrayList<String>();
+	//String []appendedWords = new String[10];
+	ArrayList<String> appendedWords = new ArrayList<String>();
 	String [] wordsToIgnore;
+	String [] words;
+	String msg;
+	int index = 0;
 	
 	KWIC(String sentences, String ignoreWords)
 	{
-		sentence = sentences.split(",");
-		wordsToIgnore = ignoreWords.split(",");
+		sentence = sentences.split(", ");
+		wordsToIgnore = ignoreWords.split(", ");
+	}
+	
+	public void append(String words[])
+	{
+		int i = 0, flag = 0;
+		for(i = 0; i < wordsToIgnore.length; i++)
+		{
+			if(words[0].equalsIgnoreCase(wordsToIgnore[i]))
+			{
+				flag ++;
+				break;
+			}
+			
+		}
+		if(flag == 0)
+		{
+			msg = String.join(" ", words);
+			appendedWords.add(msg);
+			Collections.sort(appendedWords,String.CASE_INSENSITIVE_ORDER);
+			System.out.println(appendedWords);
+		}
+	}
+	
+	public void splitIndex(String str)
+	{
+		int i, k;
+		words = str.split(" ");
+		String strTemp;
+		int count = words.length;
+		for(k = 0 ; k < count; k++)
+		{
+			strTemp = words[0];
+			for(i = 0; i < words.length - 1; i++)
+			{
+				words[i] = words[i+1];
+			}
+			words[i] = strTemp;
+		    append(words);		
+			i = 0;
+		}
+	}
+	
+	public void circularShift()
+	{
+		int i;
+		for(i = 0; i < sentence.length; i++)
+		{
+			splitIndex(sentence[i]);
+		}
+	}
+	
+	/*public void sort()
+	{
+		int i, j;
+		String temp;
+		for(i = 0; i < sentence.length; i++)
+		{
+			for(j = i+1; j < sentence.length; j++)
+			{
+				if(sentence[i].compareToIgnoreCase(sentence[j]) > 0)
+				{
+					temp = sentence[i];
+					sentence[i] = sentence[j];
+					sentence[j] = temp;
+				}
+			}
+		}
 	}
 	
 	public void print()
@@ -24,12 +98,7 @@ public class KWIC
 			System.out.println(sentence[j]);
 			j++;
 		}
-		while(i < wordsToIgnore.length)
-		{
-			System.out.println(wordsToIgnore[i]);
-			i++;
-		}
-	}
+	}*/
 
 	public static void main(String[] args) 
 	{
@@ -43,7 +112,9 @@ public class KWIC
 		ignoreWords = scanner.nextLine();
 		
 		KWIC k = new KWIC(sentences,ignoreWords);
-        k.print();
+		k.circularShift();
+		//k.sort();
+       // k.print();
 	}
 
 }
