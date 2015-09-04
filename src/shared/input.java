c5952213f9dc3072e8ea12c557f4105fc434f584
anand-sundaram <anand.sundaram@nus.edu.sg>
2015-09-04 16:26:15 +0800

    Added error handling

--- src/shared/input.java
+++ src/shared/input.java
@@ -8,24 +8,42 @@ public class input {
 		String sentences;
 		String ignoreWords;
 		int architectureType;
+		boolean isInvalid = true;
 		
-		System.out.println("Enter list of sentences:");
-		sentences = scanner.nextLine();
-		System.out.println("Enter list of words to ignore:");
-		ignoreWords = scanner.nextLine();
-		System.out.println("Enter choice of architecture 1) Shared Repository 2)Abstract Datatypes (type 1 or 2)");
-		architectureType = scanner.nextInt();
-		
-		if (architectureType == 1) {
-			MasterControl.splitter(sentences, ignoreWords);
-			MasterControl.circularShifter();
-			MasterControl.alphabetizer();
-			MasterControl.output();
-		}
-		else {
-			KWIC k = new KWIC(sentences, ignoreWords);
-			k.logic();
-	        k.print();
+		while(isInvalid) {
+			scanner = new Scanner(System.in);
+			System.out.println("Enter list of sentences:");
+			sentences = scanner.nextLine();
+			System.out.println("Enter list of words to ignore:");
+			ignoreWords = scanner.nextLine();
+			System.out.println("Enter choice of architecture 1) Shared Repository 2)Abstract Datatypes (type 1 or 2)");
+			
+			
+			if (scanner.hasNextInt()) {
+				architectureType = scanner.nextInt();
+				if (architectureType == 1) {
+					MasterControl.splitter(sentences, ignoreWords);
+					MasterControl.circularShifter();
+					MasterControl.alphabetizer();
+					MasterControl.output();
+					isInvalid = false;
+				}
+				else if (architectureType == 2) {
+					KWIC k = new KWIC(sentences, ignoreWords);
+					k.logic();
+			        k.print();
+			        isInvalid = false;
+				}
+				else {
+					System.out.println("Please enter a valid number (1 or 2)");
+				}
+			}
+			
+			else {
+				System.out.println("Please enter a number only (1 or 2)");
+			}
+			
+			
 		}
 		
 	}
